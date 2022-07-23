c97df3d80d039e66cf5d243cfcec62f94c718448
GraniteMask <ratnadeepastronomy@gmail.com>
2022-07-24 04:52:54 +0530

    react-navigation

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,10 +1,11 @@
 import { Pressable, StyleSheet, Text, View, Platform } from "react-native"
 
-function CategoryGridTile({title, color}){
+function CategoryGridTile({title, color, onPress}){
     return <View style={styles.gridItem}>
         <Pressable 
             style={({pressed})=>[styles.button, pressed ? styles.buttonPressed : null]} 
             android_ripple={{color: "#ccc"}}
+            onPress={onPress}
         >
             <View style={[styles.innerContainer, {backgroundColor: color}]}>
                 <Text style={styles.title}>{title}</Text>
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -2,11 +2,13 @@ import { FlatList } from 'react-native'
 import CategoryGridTile from '../components/CategoryGridTile'
 import { CATEGORIES } from '../data/dummy-data'
 
-function renderCategoryItem(itemData){
-    return <CategoryGridTile title={itemData.item.title} color={itemData.item.color}/>
-}
-
-function CategoriesScreen(){
+function CategoriesScreen({navigation}){
+    function renderCategoryItem(itemData){
+        function pressHandler(){
+            navigation.navigate("MealsOverview")
+        }
+        return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} onPress={pressHandler}/>
+    }
     return <FlatList data={CATEGORIES} keyExtractor={(item) => item.id} renderItem={renderCategoryItem} numColumns={2}/>
 }
 
