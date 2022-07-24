1dce06c8d3f799d3c6ff5516e092f69c36e7c2c7
GraniteMask <ratnadeepastronomy@gmail.com>
2022-07-25 04:54:11 +0530

    redux store setup

--- App.js
+++ App.js
@@ -8,6 +8,8 @@ import MealDetailScreen from './screens/MealsDetailScreen';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import FavoriteScreen from './screens/FavoriteScreen';
 import { Ionicons } from '@expo/vector-icons'
+import { Provider } from 'react-redux';
+import { store } from './store/redux/store';
 
 const Stack = createNativeStackNavigator() 
 const Drawer = createDrawerNavigator()
@@ -41,42 +43,43 @@ export default function App() {
   return (
     <>
       <StatusBar style='light' />
-      <NavigationContainer> 
-        <Stack.Navigator 
-          screenOptions={{                              // to apply in all of the screens
-          headerStyle: {backgroundColor: '#351401'},            
-          headerTintColor: 'white',
-          contentStyle: { backgroundColor: '#3f2f25' }
-        }}
-        >
-          {/* <Stack.Screen name="MealsCategories" component={CategoriesScreen} options={{
-            title: 'The Recipe Book',
-            headerTitleAlign: 'center'
-            // headerStyle: {backgroundColor: '#351401'},
-            // headerTintColor: 'white',
-            // contentStyle: { backgroundColor: '#3f2f25' }
-          }}/> */}
-          <Stack.Screen name="Drawer" component={DrawerNavigator} options={{
-            headerShown: false
-          }}/>
-          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} 
-          // options={({route, navigation}) =>{
-          //   const catId = route.params.categoryId
-          //   return{
-          //     title: catId
-          //   }
-          // }}
-          />
-          <Stack.Screen name="MealDetail" component={MealDetailScreen} 
-          // options={{
-          //   headerRight: () => {
-          //     return <Button title='In the header' />                          // This method is not useful if we want to do direct communication from the screen page itself. For that we need to
-          //   }                                                                     add these in the screen page. see the MealDetailScreen page for more info.
-          // }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-     
+      <Provider store={store}>
+        <NavigationContainer> 
+          <Stack.Navigator 
+            screenOptions={{                              // to apply in all of the screens
+            headerStyle: {backgroundColor: '#351401'},            
+            headerTintColor: 'white',
+            contentStyle: { backgroundColor: '#3f2f25' }
+          }}
+          >
+            {/* <Stack.Screen name="MealsCategories" component={CategoriesScreen} options={{
+              title: 'The Recipe Book',
+              headerTitleAlign: 'center'
+              // headerStyle: {backgroundColor: '#351401'},
+              // headerTintColor: 'white',
+              // contentStyle: { backgroundColor: '#3f2f25' }
+            }}/> */}
+            <Stack.Screen name="Drawer" component={DrawerNavigator} options={{
+              headerShown: false
+            }}/>
+            <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} 
+            // options={({route, navigation}) =>{
+            //   const catId = route.params.categoryId
+            //   return{
+            //     title: catId
+            //   }
+            // }}
+            />
+            <Stack.Screen name="MealDetail" component={MealDetailScreen} 
+            // options={{
+            //   headerRight: () => {
+            //     return <Button title='In the header' />                          // This method is not useful if we want to do direct communication from the screen page itself. For that we need to
+            //   }                                                                     add these in the screen page. see the MealDetailScreen page for more info.
+            // }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
     </>
   );
 }
