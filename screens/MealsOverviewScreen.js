c5af5a1c95da65b575da2295fda6ffc03a40d291
GraniteMask <ratnadeepastronomy@gmail.com>
2022-07-24 05:30:06 +0530

    meals-overview completed

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,5 +1,6 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, FlatList, View } from 'react-native'
 import { MEALS } from '../data/dummy-data'
+import MealItem from '../components/MealItem'
 
 // useRoute is alternative to route used here
 
@@ -7,8 +8,16 @@ function MealsOverviewScreen({ route }){                // route and navigation 
 
     const catId = route.params.categoryId
 
+    const displayedMeals = MEALS.filter((mealItem) => {
+        return mealItem.categoryIds.indexOf(catId) >= 0
+    })
+
+    function renderMealItem(itemData){
+        return <MealItem title={itemData.item.title}/>
+    }
+
     return <View style={styles.container}>
-        <Text>Meals Overview Screen - {catId}</Text>
+       <FlatList data={displayedMeals} keyExtractor={(item) => item.id} renderItem={renderMealItem}/>
     </View>
 }
 
