f88ef48a9b42b87354ab677efda69bafb5dafe12
GraniteMask <ratnadeepastronomy@gmail.com>
2022-07-24 12:32:49 +0530

    Mealitem

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,9 +1,27 @@
-import { Text, View } from 'react-native'
+import { Image, Pressable, Text, View, StyleSheet } from 'react-native'
 
-function MealItem({title}){
+function MealItem({title, imageUrl}){
     return <View>
-        <Text>{title}</Text>
+        <Pressable>
+            <View>
+                <Image source={{ uri: imageUrl}} style={styles.image}/>
+                <Text style={styles.title}>{title}</Text>
+            </View>
+        </Pressable>
+        
     </View>
 }
+
+const styles = StyleSheet.create({
+    image:{
+        width: '100%',
+        height: 200,
+    },
+    title:{
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontSize: 18
+    }
+})
 
 export default MealItem
